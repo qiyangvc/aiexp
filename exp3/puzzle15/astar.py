796bae3a4ea9b5ead4d710792b305374772bf326
qiyangvc <1822711060@qq.com>
2025-04-11 20:31:12 +0800

    不需要f(x)的astar算法

--- exp3/puzzle15/astar.py
+++ exp3/puzzle15/astar.py
@@ -1,62 +1,70 @@
 from queue import  PriorityQueue
 import copy
-G={}
+Qs=[]
 Q=PriorityQueue(0)
 def F(puzzle):
-    global G
-    return G[puzzle]+H(puzzle)
+    return H(puzzle)
 
-def A_star(puzzle):
-    global G   
-    G={tuple(puzzle):0}# f=g+h
+def A_star(puzzle):   # f=g+h
+    global Q
+    global Qs
+    Qs.append(puzzle)
     Q.put((F(puzzle),puzzle))
-    while not Q.empty:
-        puzzles=Q.get()
-        mymove(puzzles[0][1])
+    while not Q.empty():
+        t_puzzle=Q.get()
+        mymove(t_puzzle[1])
+    return 
 
     
     
-def mymove(puzzle,Q):
-    global G
+def mymove(puzzle):
+    global Q
+    global Qs
     if is_finished(puzzle):
         return
     i=0
     j=0
+    k=0
     while i<len(puzzle):
+        j=0
         while j<len(puzzle[0]):
             if puzzle[i][j]==0:
+                k=1
                 break
             j+=1
-        if puzzle[i][j]==0:
+        if k:
             break
         i+=1
     if(i!=len(puzzle)-1):
         puzzle1=copy.deepcopy(puzzle)
         puzzle1[i][j],puzzle1[i+1][j]=puzzle1[i+1][j],puzzle1[i][j]
-        if tuple(puzzle1) not in G:
+        if puzzle1 not in Qs:
             Q.put((F(puzzle1),puzzle1))
-            G[tuple(puzzle1)]=G[tuple(puzzle)]+1
+            Qs.append(puzzle1)
+        
     if(i!=0):
         puzzle2=copy.deepcopy(puzzle)
         puzzle2[i][j],puzzle2[i-1][j]=puzzle2[i-1][j],puzzle2[i][j]
-        Q.put((F(puzzle2),puzzle2))
-        if tuple(puzzle2) not in G:
+        if puzzle2 not in Qs:
             Q.put((F(puzzle2),puzzle2))
-            G[tuple(puzzle2)]=G[tuple(puzzle)]+1
+            Qs.append(puzzle2)
+
     if(j!=len(puzzle[0])-1):
         puzzle3=copy.deepcopy(puzzle)
         puzzle3[i][j],puzzle3[i][j+1]=puzzle[i][j+1],puzzle[i][j]
-        Q.put((F(puzzle3),puzzle3))
-        if tuple(puzzle3) not in G:
-            Q.put((F(puzzle1),puzzle1))
-            G[tuple(puzzle1)]=G[tuple(puzzle)]+1
+        if puzzle3 not in Qs:
+            Q.put((F(puzzle3),puzzle3))
+            Qs.append(puzzle3)
+
     if(j!=0):
         puzzle4=copy.deepcopy(puzzle)
         puzzle[i][j],puzzle[i][j-1]=puzzle[i][j-1],puzzle[i][j]
-        Q.put((F(puzzle4),puzzle4))
-        if tuple(puzzle4) not in G:
-            Q.put((F(puzzle1),puzzle1))
-            G[tuple(puzzle4)]=G[tuple(puzzle)]+1
+        if puzzle4 not in Qs:
+            Q.put((F(puzzle4),puzzle4))
+            Qs.append(puzzle4)
+
+
+
     
 
     
@@ -69,8 +77,9 @@ def H(puzzle):
             p_value=puzzle[i][j]
             if p_value==0:
                 p_value=16
-            ri=p_value/len(puzzle)
-            rj=p_value%len(puzzle)
+            p_value-=1
+            ri=int(p_value/len(puzzle))
+            rj=int(p_value%len(puzzle))
             c+=abs(i-ri)+abs(j-rj)
             j+=1
         i+=1
@@ -80,10 +89,14 @@ def is_finished(puzzle):
     while i<len(puzzle):
         j=0
         while j<len(puzzle[0]):
-            if i*len(puzzle)+j+1!=puzzle[i][j]:
+            p_value=puzzle[i][j]
+            if p_value==0:
+                p_value=16
+            if i*len(puzzle)+j+1!=p_value:
                 return False
             j+=1
         i+=1
+    print("succes")
     return True
 
 puzzle=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[0,13,14,15]]
